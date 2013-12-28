788832e124dcbb126ddd8009a648736aae9990ab
Dave Hill <admin@digitalbrook.com>
2013-12-28 11:18:30 -0500

    Consolidated JavaScript files.

--- js/custom.js
+++ js/custom.js
@@ -164,6 +164,48 @@ $(document).ready(function(){
 });
 
 
+$(window).load(function() {
+   $('#featured-page-title').orbit({
+	   animation: 'fade',                  // fade, horizontal-slide, vertical-slide, horizontal-push
+	   animationSpeed: 1200,                // how fast animations are
+	   timer: true, 			 // true or false to have the timer
+	   advanceSpeed: 4000, 		 // if timer is enabled, time between transitions 
+	   pauseOnHover: false, 		 // if you hover pauses the slider
+	   startClockOnMouseOut: false, 	 // if clock should start on MouseOut
+	   startClockOnMouseOutAfter: 1000, 	 // how long after MouseOut should the timer start again
+	   directionalNav: false, 		 // manual advancing directional navs
+	   captions: false, 			 // do you want captions?
+	   captionAnimation: 'fade', 		 // fade, slideOpen, none
+	   captionAnimationSpeed: 800, 	 // if so how quickly should they animate in
+	   bullets: false,			 // true or false to activate the bullet navigation
+	   bulletThumbs: false,		 // thumbnails for the bullets
+	   bulletThumbLocation: '',		 // location from this file where thumbs will be
+	   afterSlideChange: function(){} 	 // empty function 
+	});
+});
+
+
+//View/Hide Thumbnail JavaScript
+$(document).ready(function() { 
+  $(".panel").hide();
+  $("#featured-main-slider").hover( 
+    function(){ 
+			$(".panel").stop(true, true).slideDown('medium'); 
+		},
+    function(){ 
+			$(".panel").slideUp('medium'); 
+		}
+  );
+});
+
+
+$(document).ready(function() {
+  $('#mycarousel').jcarousel({
+    scroll: 2, // The number of items to scroll by. 
+		animation: "slow" //("slow" or "fast")
+  });
+});
+
 (function($) {
     $.fn.tipsy = function(options) {
 
--- js/mycarousel.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $('#mycarousel').jcarousel({
-    scroll: 2, // The number of items to scroll by. 
-		animation: "slow" //("slow" or "fast")
-  });
-});
